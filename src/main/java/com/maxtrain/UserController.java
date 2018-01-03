c2f08a047f2ad07ab0f01b4eb6897daaee51537b
Greg doud <gpdoud@gmail.com>
2018-01-02 22:56:40 -0500

    Updated UserController.java

--- src/main/java/com/maxtrain/UserController.java
+++ src/main/java/com/maxtrain/UserController.java
@@ -5,12 +5,13 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.maxtrain.utility.JsonResponse;
 
 @CrossOrigin
-@RestController
+@Controller
 @RequestMapping("/Users")
 public class UserController {
 	
